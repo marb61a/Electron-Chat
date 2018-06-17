b1b73f31af0c214c3bc11201c0ef7e19be7a5177
marb61a <marb61a@gmail.com>
2018-06-17 12:47:35 +0100

    added signup functionality auth.service

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -32,7 +32,7 @@ export class SignupPageComponent implements OnInit {
   }
 
   createAccount() {
-    
+
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,17 +3,40 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
 import { Router } from '@angular/router';
 
+import { constants } from '../constants';
+
 @Injectable()
 export class AuthService {
   private authState: any;
 
   constructor(
-    private afAuth: AngularFireAuth,
-    private afStore: AngularFirestore,
+    private afauth: AngularFireAuth,
+    private afs: AngularFirestore,
     private router: Router
-  ) { }
+  ) {
+    this.afauth.authState.subscribe((user) => {
+      this.authState = user;
+    });
+  }
 
-  signup(usercreads){
+  get currentUserId(): string {
+    return this.authState !== null ? this.authState.uid : '';
+  }
 
+  signup(usercreds) {
+    return this.afauth.auth.createUserWithEmailAndPassword(usercreds.email, usercreds.password)
+      .then((user) => {
+        this.authState = user;
+        this.afauth.auth.currentUser.updateProfile({
+          displayName: usercreds.displayName,
+          photoURL: constants.PROFILE_PIC
+        }).then(() => {
+          this.setUserData(usercreds.email, usercreds.displayName, user.photoURL);
+        });
+      });
+  }
+
+  setUserData(email: string, displayName: string, photoURL: string) {
+    const path = `/users/${this.currentUserId}`;
   }
 }
