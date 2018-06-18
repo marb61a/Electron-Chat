201db43d6c1130ef08776b6f6c6b0e3075f5c98b
marb61a <marb61a@gmail.com>
2018-06-18 12:50:06 +0100

    added profile component and updated dashboard html with layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { AuthService } from './services/auth.service';
 import { AuthguardService } from './services/authguard.service';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     SignupPageComponent,
     DashboardComponent,
     NavBarComponent,
-    SidebarComponent
+    SidebarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
