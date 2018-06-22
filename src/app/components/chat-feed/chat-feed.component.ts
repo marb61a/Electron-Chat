6fa484b1562f491af9d27e51335f26d83860ed29
marb61a <marb61a@gmail.com>
2018-06-22 16:59:42 +0100

    added getMessages method to chat-feed ts

--- src/app/components/chat-feed/chat-feed.component.ts
+++ src/app/components/chat-feed/chat-feed.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessagesService } from '../../services/messages.service';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-chat-feed',
   templateUrl: './chat-feed.component.html',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatFeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messagesService: MessagesService, private authService: AuthService) { }
+
+  showChat: boolean = false;
+  messages = [];
+  loadingSpinner: boolean = false;
+  MyId;
+  MyAvatar;
+  currentChatUser;
 
   ngOnInit() {
+    this.messagesService.enteredChat.subscribe((value: any) => {
+      this.showChat = value;
+      this.getMessages();
+      this.currentChatUser = this.messagesService.currentChatUser;
+    });
+    this.MyAvatar = this.authService.currentUserDetails();
   }
 
+  getMessages() {
+    this.loadingSpinner = true;
+    this.messagesService.getAllMessages().then((messageObs: any) => {
+      if(!messageObs) {
+        this.loadingSpinner = false;
+        this.messages = [];
+        console.log('Nothing To Show');
+      } else {
+        messageObs.subscribe((messages) => {
+          this.loadingSpinner = false;
+          this.messages = messages;
+        });
+      }
+    });
+  }
 }
