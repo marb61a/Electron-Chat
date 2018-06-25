34dcab4ce6a26af624a06cbd50b419637875be3e
marb61a <marb61a@gmail.com>
2018-06-25 17:02:59 +0100

    update pipes

--- src/app/pipes/smart-date.pipe.ts
+++ src/app/pipes/smart-date.pipe.ts
@@ -2,15 +2,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 import { RelativeDatePipe } from './relative-date.pipe';
-const secondsInADay = 86400;
+const secondsInAday = 86400;
 
 @Pipe({
   name: 'smartDate'
 })
 export class SmartDatePipe implements PipeTransform {
-
   transform(dateStamp: number, relativeMax: number = 10): string {
-    return null;
-  }
+    let timeAgoInSeconds = Math.floor((Date.now() - new Date(dateStamp).getTime()) / 1000);
+
+    if (timeAgoInSeconds < relativeMax * secondsInAday) {
+        return new RelativeDatePipe().transform(dateStamp);
+    } else {
+        return new Date(dateStamp).toLocaleDateString('en-GB');
+    }
+
+}
 
 }
